fc1fba3df313efe09ed2c9e0ac4a205716ce7080
Brian <brian.webb8@yahoo.com>
2016-12-20 15:46:19 -0600

    Made resume section mobile friendly

--- personal_js/personal_site.js
+++ personal_js/personal_site.js
@@ -1,6 +1,6 @@
 (function () {
     "use strict";
-//-----------------------------------------navigation tab functionality
+//-----------------------------------------resume tab functionality
     $('.nav-tabs a').click(function (e) {
         e.preventDefault();
         $(this).tab('show')
